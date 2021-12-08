0f4f38ea9ca80e758423c887dc47143cfaa1ab00
Chris Trzesniewski <k.trzesniewski@gmail.com>
2021-12-08 08:47:50 +0100

    day 8 - solution cleanup

--- 2021/day8/task2.ts
+++ 2021/day8/task2.ts
@@ -2,11 +2,93 @@ import { input } from './input2'
 import { toNumber } from '../../utils'
 import { add } from 'ramda'
 
-const containsStr = (check: string, str: string) =>
-  str.split('').every((char) => check.includes(char))
+const defaultMapping = {
+  abcefg: 0,
+  cf: 1,
+  acdeg: 2,
+  acdfg: 3,
+  bcdf: 4,
+  abdfg: 5,
+  abdefg: 6,
+  acf: 7,
+  abcdefg: 8,
+  abcdfg: 9,
+}
 
-const solution = (solutionInput: string) => {
-  return solutionInput
+const ONE = 'cf'
+const MIDDLE_DASH = 'd'
+const DIFF_FOUR_AND_ONE = 'bd'
+
+const containsStr = (valueString: string, searchString: string) =>
+  searchString.split('').every((char) => valueString.includes(char))
+
+const getPossibleValues = (value: string) => {
+  return Object.entries(defaultMapping)
+    .filter(([key]) => key.length === value.length)
+    .map(([_, value]) => value)
+}
+
+const diffStrings = (from: string, to: string) =>
+  to
+    .split('')
+    .filter((c) => !from.split('').includes(c))
+    .join('')
+
+const calculateDMappingIfPossible = (mapping: Record<string, string>) => {
+  if (!mapping[MIDDLE_DASH] && mapping[2] && (mapping[2] || mapping[3])) {
+    const [b, d] = mapping[DIFF_FOUR_AND_ONE].split('')
+    const value = mapping[2] || mapping[3]
+    mapping[MIDDLE_DASH] = value.includes(b) ? b : d
+  }
+}
+
+const deduceMapping = (trainingData: string) => {
+  const possibleValues = trainingData.split(' ').map((value) => ({
+    pattern: value,
+    values: getPossibleValues(value),
+  }))
+
+  const basicMapping = possibleValues
+    .filter(({ values }) => values.length === 1)
+    .reduce<Record<number, string>>(
+      (res, { values: [v], pattern }) => ({
+        ...res,
+        [v]: pattern,
+      }),
+      {},
+    )
+
+  const mapping: Record<string, string> = {
+    a: diffStrings(basicMapping[1], basicMapping[7]),
+    cf: basicMapping[1],
+    bd: diffStrings(basicMapping[1], basicMapping[4]),
+    eg: diffStrings(basicMapping[4] + basicMapping[7], basicMapping[8]),
+  }
+
+  trainingData
+    .split(' ')
+    .map((value) => ({
+      key: value,
+      values: getPossibleValues(value),
+    }))
+    .forEach(({ key }) => {
+      calculateDMappingIfPossible(mapping)
+      if (key.length === 5) {
+        if (containsStr(key, mapping[ONE])) {
+          mapping[3] = key
+        } else if (containsStr(key, mapping[DIFF_FOUR_AND_ONE])) {
+          mapping[5] = key
+        } else {
+          mapping[2] = key
+        }
+      }
+    })
+
+  return mapping
+}
+
+const solution = (solutionInput: string) =>
+  solutionInput
     .trim()
     .split('\n')
     .map((line) => {
@@ -15,30 +97,28 @@ const solution = (solutionInput: string) => {
 
       return inputData
         .split(' ')
-        .map((s) => {
-          return {
-            key: sortStringWithMapping(s),
-            values: Object.entries(defaultMapping)
-              .filter(([key]) => key.length === s.length)
-              .map(([_, value]) => value),
-          }
-        })
+        .map((value) => ({
+          key: value,
+          values: getPossibleValues(value),
+        }))
         .map(({ key, values }) => {
           switch (key.length) {
             case 5:
-              if (containsStr(key, mapping['cf'])) {
+              if (containsStr(key, mapping[ONE])) {
                 return 3
-              } else if (containsStr(key, mapping['bd'])) {
-                return 5
               } else {
-                return 2
+                if (containsStr(key, mapping[DIFF_FOUR_AND_ONE])) {
+                                return 5
+                              } else {
+                                return 2
+                              }
               }
             case 6: {
               calculateDMappingIfPossible(mapping)
-              if (mapping['d']) {
-                if (!containsStr(key, mapping['d'])) {
+              if (mapping[MIDDLE_DASH]) {
+                if (!containsStr(key, mapping[MIDDLE_DASH])) {
                   return 0
-                } else if (containsStr(key, mapping['cf'])) {
+                } else if (containsStr(key, mapping[ONE])) {
                   return 9
                 } else {
                   return 6
@@ -55,101 +135,5 @@ const solution = (solutionInput: string) => {
     })
     .map(toNumber)
     .reduce(add, 0)
-}
 
-const defaultMapping = {
-  abcefg: 0,
-  cf: 1,
-  acdeg: 2,
-  acdfg: 3,
-  bcdf: 4,
-  abdfg: 5,
-  abdefg: 6,
-  acf: 7,
-  abcdefg: 8,
-  abcdfg: 9,
-}
-
-const sortStrMap: Record<string, number> = {
-  a: 1,
-  b: 2,
-  d: 3,
-  c: 4,
-  f: 5,
-  e: 7,
-  g: 7,
-}
-
-const sortString = (str: string) => str.split('').sort().join('')
-const sortStringWithMapping = (str: string) =>
-  str
-    .split('')
-    .sort((a, b) => sortStrMap[a] - sortStrMap[b])
-    .join('')
-
-const diffStrings = (from: string, to: string) =>
-  to
-    .split('')
-    .filter((c) => !from.split('').includes(c))
-    .join('')
-
-function calculateDMappingIfPossible(mapping: Record<string, string>) {
-  if (!mapping['d'] && mapping[2] && (mapping[2] || mapping[3])) {
-    const [b, d] = mapping['bd'].split('')
-    const value = mapping[2] || mapping[3]
-    mapping['d'] = value.includes(b) ? b : d
-  }
-}
-
-const deduceMapping = (line: string) => {
-  const possibleValues = line.split(' ').map((s) => {
-    return {
-      pattern: sortString(s),
-      values: Object.entries(defaultMapping)
-        .filter(([key]) => key.length === s.length)
-        .map(([_, value]) => value),
-    }
-  })
-
-  const basicMapping = possibleValues
-    .filter(({ values }) => values.length === 1)
-    .reduce<Record<number, string>>((res, { values: [v], pattern }) => {
-      return {
-        ...res,
-        [v]: pattern,
-      }
-    }, {})
-
-  const mapping: Record<string, string> = {
-    a: sortStringWithMapping(diffStrings(basicMapping[1], basicMapping[7])), // ?
-    cf: sortStringWithMapping(basicMapping[1]),
-    bd: sortStringWithMapping(diffStrings(basicMapping[1], basicMapping[4])), // ?
-    eg: sortStringWithMapping(
-      diffStrings(basicMapping[4] + basicMapping[7], basicMapping[8]),
-    ),
-  }
-
-  line
-    .split(' ')
-    .map((s) => ({
-      key: sortStringWithMapping(s),
-      values: Object.entries(defaultMapping)
-        .filter(([key]) => key.length === s.length)
-        .map(([_, value]) => value),
-    }))
-    .forEach(({ key }) => {
-      calculateDMappingIfPossible(mapping)
-      if (key.length === 5) {
-        if (containsStr(key, mapping['cf'])) {
-          mapping[3] = key
-        } else if (containsStr(key, mapping['bd'])) {
-          mapping[5] = key
-        } else {
-          mapping[2] = key
-        }
-      }
-    })
-
-  return mapping
-}
 console.log(solution(input))
